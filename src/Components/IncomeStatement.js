9c4a2af18b75befdb5164b8388c85515e78fb0af
Ali <2085836@cognizant.com>
2022-06-28 15:23:10 +0530

    first-commit

---
+++ src/Components/IncomeStatement.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class IncomeStatement extends Component {
+    constructor(props){
+        super(props)
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <section className='container-fluid p-4'> 
+                <h4>Income Statement</h4>
+            </section>
+        );
+    }
+}
